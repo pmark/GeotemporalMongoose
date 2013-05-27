fe8971db43293a155d3d38fe2c7f1a5ebee43f3a
P. Mark Anderson <mark@spotmetrix.com>
2013-05-26 19:55:40 -0700

    Refactored to find nearby tweets already in DB first, then call twitter with sinceId, but findNear is broken.

--- app.js
+++ app.js
@@ -113,135 +113,142 @@ app.configure(function() {
     app.use(app.router);
 });
 
-// range is in km
+// Range is in km
 app.get("/twitter/:latitude/:longitude/:range", function(req, res) {
 
     // Search for results at this location.
 
-    var limit = req.query.limit || 100;
-    var sinceId = null;
-
-    tweetFetch.search(req.params.latitude, req.params.longitude, req.params.range, limit, sinceId, function(data) {
-
-        var responseData = {
+    var limit = (req.query.limit || 100),
+        sinceId = null,
+        responseData = {
             tweets: []
         };
 
-        if (data && data.statuses && data.statuses.length) {
+    console.log("Searching for tweets near lat/lng ", req.params.latitude, req.params.longitude);
 
-            responseData.code = 200;
-            responseData.msg = "OK";
-
-            responseData.tweets = [];
-            responseData.statuses = [];
-
-            var newTweetData = [];
-            var oldTweetData = [];
-            var fetchedTweetIds = [];
-            var existingTweetIds = [];
-
-            // Find existing tweets in our DB. Don't duplicate data.
-
-            data.statuses.forEach(function(tweetData) {
-                console.log("fetched tweet", tweetData.id);
-                fetchedTweetIds.push(tweetData.id);
-            });
-
-            Tweets.find({'tweet_id': {$in: fetchedTweetIds} }).exec(function(err, result) { 
-                if (err) {
-                    console.log("Error fetching tweet:", err);
-                }
-                else {
-
-                    if (result) {
-                        console.log(result.length, "existing tweets found");
-
-                        result.forEach(function(tweet) {
-                            responseData.tweets.push(tweet);
-                            existingTweetIds.push(tweet.tweet_id);
-                            console.log(tweet.tweet_id, "is an old tweet by", tweet.username);
-                        });
-                    }
-                }
-
-                data.statuses.forEach(function(tweetData) {
-                    if (existingTweetIds.indexOf(tweetData.id) != -1) {
-                        console.log(tweetData.id, "Skipping extant tweet by", tweetData.user.screen_name);
-                        return;
-                    }
-                    else {
-                        console.log(tweetData.id, "is a new tweet by", tweetData.user.screen_name);
-                    }
-
-                    var userData = tweetData.user || {};
-                    var latitude = null;
-                    var longitude = null;
-                    var coordData = tweetData.geo ? tweetData.geo.coordinates : null;
-
-                    if (coordData) {
-                        // The 'geo' property exists.
-                        latitude = coordData[0];
-                        longitude = coordData[1];
-                    }
-                    else {
-                        // Try using the 'coordinates' property.
-                        coordData = tweetData.coordinates;
-
-                        if (coordData) {
-                            longitude = coordData[0];
-                            latitude = coordData[1];
-                        }
-                    }
-
-                    if (coordData && coordData.length) {
-
-                        // Save the geotagged tweet.
-
-                        // responseData.statuses.push(tweetData);
-                        newTweetData.push({
-                            tweet_id: tweetData.id,
-                            text: tweetData.text,
-                            user_full_name: userData.name,
-                            username: userData.screen_name,
-                            geo: {
-                                type: "Point",
-                                coordinates: [longitude, latitude]
-                            }
-                        });
-                    }
+    findNear(
+        req.params.latitude, 
+        req.params.longitude, 
+        req.params.range, 
+        limit, 
+        function(err, docs) {
+            if (docs && docs.results && docs.results.length) {
+                console.log(docs.results.length, "tweets found near lat/lng", req.params.latitude, req.params.longitude);
+                
+                docs.results.forEach(function(tmpTweet) {
+                    responseData.tweets.push(tmpTweet);
+                    sinceId = Match.max(sinceId, tmpTweet.tweet_id);
+                    console.log(tmpTweet.tweet_id, "is an old tweet by", tmpTweet.username);
                 });
+            }
+            else {
+                console.log("No tweets found near lat/lng ", req.params.latitude, req.params.longitude);
+            }
 
-                if (newTweetData && newTweetData.length > 0) {
-                    Tweets.create(newTweetData, function (err, results) {
-                        if (err) {
-                            console.log('Error saving tweets:', err);
-                        }
-                        else {
+            tweetFetch.search(
+                req.params.latitude, 
+                req.params.longitude,
+                req.params.range,
+                limit,
+                sinceId, 
+                function(data) {
+                    if (data && data.statuses && data.statuses.length) {
+                        responseData.code = 200;
+                        responseData.msg = "OK";
 
-                            // TODO:  add _id to each tweet
+                        responseData.tweets = [];
 
-                            newTweetData.forEach(function(tmpTweet) {
-                                console.log(tmpTweet.tweet_id, "is a new tweet by", tmpTweet.username);
-                                responseData.tweets.push(tmpTweet);
+                        var newTweetData = [];
+                        var oldTweetData = [];
+                        var fetchedTweetIds = [];
+                        var existingTweetIds = [];
+
+                        data.statuses.forEach(function(tweetData) {
+                            fetchedTweetIds.push(tweetData.id);
+                        });
+
+                        Tweets.find({'tweet_id': {$in: fetchedTweetIds} }).exec(function(err, docs) { 
+                            if (err) {
+                                console.log("Error querying existing tweets:", err);
+                            }
+                            else {
+                                if (docs) {
+                                    docs.forEach(function(tweet) {
+                                        responseData.tweets.push(tweet);
+                                        existingTweetIds.push(tweet.tweet_id);
+                                        // console.log(tweet.tweet_id, "is an old tweet by", tweet.username);
+                                    });
+                                }
+                            }
+
+                            data.statuses.forEach(function(tweetData) {
+                                if (existingTweetIds.indexOf(tweetData.id) != -1) {
+                                    console.log(tweetData.id, "Skipping extant tweet by", tweetData.user.screen_name);
+                                    return;
+                                }
+                                else {
+                                    console.log(tweetData.id, "is a new tweet by", tweetData.user.screen_name);
+                                }
+
+                                var latitude = null;
+                                var longitude = null;
+                                var userData = tweetData.user || {};
+                                var coordData = tweetData.geo ? tweetData.geo.coordinates : null;
+
+                                if (coordData) {
+                                    latitude = coordData[0];
+                                    longitude = coordData[1];
+                                }
+                                else {
+                                    coordData = tweetData.coordinates;
+
+                                    if (coordData) {
+                                        longitude = coordData[0];
+                                        latitude = coordData[1];
+                                    }
+                                }
+
+                                if (coordData && coordData.length) {
+                                    var tmpTweet = {
+                                        tweet_id: tweetData.id,
+                                        text: tweetData.text,
+                                        user_full_name: userData.name,
+                                        username: userData.screen_name,
+                                        geo: {
+                                            type: "Point",
+                                            coordinates: [longitude, latitude]
+                                        }
+                                    };
+
+                                    responseData.tweets.push(tmpTweet);
+                                    newTweetData.push(tmpTweet);
+                                }
                             });
-                        }
 
-                        res.json(responseData);
-                    });
+                            res.json(responseData);
+
+                            if (newTweetData && newTweetData.length) {
+                                Tweets.create(newTweetData, function (err, results) {
+                                    if (err) {
+                                        console.log('Error saving tweets:', err);
+                                    }
+                                    else {
+                                        newTweetData.forEach(function(tmpTweet) {
+                                            console.log(tmpTweet.tweet_id, "is a new tweet by", tmpTweet.username);
+                                        });
+                                    }
+                                });
+                            }
+                        }); // end Tweets.find
+
+                    }
+                    else {
+                        console.log("No new tweets.")
+                    }
                 }
-                else {
-                    // All tweets already existed
-
-                    console.log("All tweets already existed");
-                    res.json(responseData);
-                }
-            });
-
-            // console.log("\n\nsending response:\n\n", responseData);
-                    
+            ); // end tweetFetch
         }
-    });
-
+    );  // end findNear
 });
 
 app.get("/all", function(req, res) {
@@ -292,16 +299,16 @@ app.get("/test", function(req, res) {
 });
 
 
-function findNear(latitude, longitude, meters, limit, done) {
+function findNear(latitude, longitude, km, limit, done) {
     // See http://docs.mongodb.org/manual/reference/command/geoNear/#dbcmd.geoNear
 
-    meters = meters || 16093; // 10 miles
+    km = km || 15; // 10 miles
     limit = limit || 100;
 
     // db.runCommand( { geoNear: "tweets", near: [ -122.6750, 45.5236 ], spherical:true } )
     Tweets.collection.geoNear(longitude, latitude, {
         spherical: true, 
-        maxDistance: meters
+        maxDistance: (km * 1000)
     }, 
     done);    
     // function(err, docs) {
@@ -311,7 +318,7 @@ function findNear(latitude, longitude, meters, limit, done) {
     //     }
     // });
 
-    // Tweets.find( { location: { $near: [longitude, latitude] , $maxDistance: meters } }, done );
+    // Tweets.find( { location: { $near: [longitude, latitude] , $maxDistance: km } }, done );
     return;
 
     // return Tweets.find({
@@ -328,7 +335,7 @@ function findNear(latitude, longitude, meters, limit, done) {
     //         $geoNear: {
     //             $spherical: true,
     //             $includeLocs: true,
-    //             $maxDistance: meters,
+    //             $maxDistance: km,
     //             $limit: limit,
     //             $near: { 
     //                 coordinates: [ longitude, latitude ]
